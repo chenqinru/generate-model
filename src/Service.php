fd0ec2abec277f090b1e2c4fb8f6bb87a87af78e
chenqinru <chenqinru0@gmail.com>
2021-04-06 17:05:10 +0800

    commit

--- src/Service.php
+++ src/Service.php
@@ -1,40 +1,20 @@
 <?php
 
-namespace think\queue;
+declare(strict_types=1);
 
-use Generate\Model;
-use think\helper\Arr;
-use think\helper\Str;
-use think\queue\command\FailedTable;
-use think\queue\command\FlushFailed;
-use think\queue\command\ForgetFailed;
-use think\queue\command\Listen;
-use think\queue\command\ListFailed;
-use think\queue\command\Restart;
-use think\queue\command\Retry;
-use think\queue\command\Table;
-use think\queue\command\Work;
+namespace Generate;
 
+/**
+ * @note   Service
+ * @author CQR
+ * @date   2021/4/6 17:04
+ */
 class Service extends \think\Service
 {
-    public function register()
-    {
-        $this->app->bind('queue', Queue::class);
-        $this->app->bind(
-            'queue.failer',
-            function () {
-                $config = $this->app->config->get('queue.failed', []);
-
-                $type = Arr::pull($config, 'type', 'none');
-
-                $class = false !== strpos($type, '\\') ? $type : '\\think\\queue\\failed\\' . Str::studly($type);
-
-                return $this->app->invokeClass($class, [$config]);
-            }
-        );
-    }
-
-    public function boot()
+    /**
+     * @inheritDoc
+     */
+    public function boot(): void
     {
         $this->commands(['model' => Model::class]);
     }
