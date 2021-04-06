9e531323dc50dc0f656860c6370b9c57670c0f35
chenqinru <chenqinru0@gmial.com>
2021-04-06 22:43:00 +0800

    commit

---
+++ tests/GenerateModelTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @note   GenerateModelTest
+ * @author CQR
+ * @date   2021/4/6 22:34
+ */
+class GenerateModelTest extends TestCase
+{
+    public function test()
+    {
+        print_r(1231231);
+    }
+}
